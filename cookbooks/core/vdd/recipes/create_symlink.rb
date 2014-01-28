9f0b01569601c5f31357352cf58d47f61978b3ec
Gerald Villorente <geraldvillorente@gmail.com>
2014-01-29 04:54:25 +0800

    Update Chef recipe to handle the installation of Coder and Coder Review modules

--- cookbooks/core/vdd/recipes/create_symlink.rb
+++
@@ -1,6 +0,0 @@
-# Create Drupal coding standard symlink inside PHP Codesniffer
-link "/usr/share/php/PHP/CodeSniffer/Standards/Drupal" do
-  to "/vagrant/bin/coder_module/coder_sniffer/Drupal"
-  link_type :symbolic
-  action :create
-end

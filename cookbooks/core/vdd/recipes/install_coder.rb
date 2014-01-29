baa765cabe3002a243deb9087319c9749ef907de
Gerald Villorente <geraldvillorente@gmail.com>
2014-01-30 03:29:26 +0800

    Update install_coder recipe to be more generic

--- cookbooks/core/vdd/recipes/install_coder.rb
+++ cookbooks/core/vdd/recipes/install_coder.rb
@@ -1,27 +1,33 @@
-# Create Drupal coding standard symlink inside PHP Codesniffer
-link "/usr/share/php/PHP/CodeSniffer/Standards/Drupal" do
-  to "/vagrant/bin/coder_module/coder_sniffer/Drupal"
-  link_type :symbolic
-  action :create
-end
+if node["sites"]
+  node["sites"].each do |index, site|
 
-# Create symlink of coder_module inside Drupal contrib directory
-link "/var/www/drupal7/sites/all/modules/contrib/coder_module" do
-  to "/vagrant/bin/coder_module"
-  link_type :symbolic
-  action :create
-end
+    # Create Drupal coding standard symlink inside PHP Codesniffer
+    link "/usr/share/php/PHP/CodeSniffer/Standards/Drupal" do
+      to "/vagrant/bin/coder_module/coder_sniffer/Drupal"
+      link_type :symbolic
+      action :create
+    end
 
-# Enable the coder module using Drush
-execute "install_coder" do
-  cwd '/var/www/drupal7/sites/vdd'
-  command "drush en coder --yes"
-  action :run
-end
+    # Create symlink of coder_module inside Drupal contrib directory
+    link "/var/www/#{index}/sites/all/modules/contrib/coder_module" do
+      to "/vagrant/bin/coder_module"
+      link_type :symbolic
+      action :create
+    end
 
-# Enable the coder module using Drush
-execute "install_coder_review" do
-  cwd '/var/www/drupal7/sites/vdd'
-  command "drush en coder_review --yes"
-  action :run
+    # Enable the coder module using Drush
+    execute "install_coder" do
+      cwd "/var/www/#{index}/sites/vdd"
+      command "drush en coder --yes"
+      action :run
+    end
+
+    # Enable the coder module using Drush
+    execute "install_coder_review" do
+      cwd "/var/www/#{index}/sites/vdd"
+      command "drush en coder_review --yes"
+      action :run
+    end
+
+  end
 end
